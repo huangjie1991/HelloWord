f2abc04a61acdd2c424b3df74e32b2de62374056
huangjie <1@QQ.COM>
2020-04-13 16:29:49 +0800

    提交

--- src/main/java/com/demo/app/SpringConfigApplication.java
+++ src/main/java/com/demo/app/SpringConfigApplication.java
@@ -1,9 +1,14 @@
 package com.demo.app;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+/**
+ * 配置中心
+ */
+@EnableConfigServer
+@SpringCloudApplication
 public class SpringConfigApplication {
 
 	public static void main(String[] args) {
